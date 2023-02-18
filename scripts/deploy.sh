99226072cd23e783f320705d0e0fd5524f123c7b
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-02-18 21:23:26 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,6 @@
 python3 -m venv venv
 source venv/bin/activate
 
-# Install dependencies
-pip install -r requirements.txt
-
 # Run Python script to retrieve OpenAI models and generate HTML output
 python app.py
 
